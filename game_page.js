ca523ca53d42f2b31caa39c0f8f11229849b11cb
soupman2 <150656004+soupman2@users.noreply.github.com>
2024-05-16 20:19:50 +1000

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,12 @@
+p1name=localStorage.getItem("player1name");
+p2name=localStorage.getItem("player2name");
+p1score=0;
+p2score=0;
+questionturn="p1";
+answerturn="p2";
+document.getElementById("player1_name").innerHTML=p1name;
+document.getElementById("player2_name").innerHTML=p2name;
+document.getElementById("player1_score").innerHTML=p1score;
+document.getElementById("player2_score").innerHTML=p2score;
+document.getElementById("player_question").innerHTML="Question turn:"+p1name;
+document.getElementById("player_answer").innerHTML="Answer turn:"+p2name;
